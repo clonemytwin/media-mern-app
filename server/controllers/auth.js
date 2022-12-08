58422afbd4b98e838418a8d1fdb3bad9d03ade6b
clonemytwin <63527980+clonemytwin@users.noreply.github.com>
2022-12-08 09:15:53 +0600

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,12 +2,35 @@ import bcrypt from "bcrypt";
 import { Jwt } from "jsonwebtoken";
 import User from "../model/User.js";
 
-
 //register
 
-export const register = async(req, res) =>
-{
-    try{
-        {}
-    }
-}
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 100),
+      impressions: Math.floor(Math.random() * 100),
+    });
+  } catch (err) {}
+};
